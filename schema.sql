de3ee60de27472db5c5b2243d5d747ef18a50224
mhitchi <mayhitchings@gmail.com>
2020-02-08 16:46:01 -0500

    added commented plan into empty functions

--- schema.sql
+++ schema.sql
@@ -24,6 +24,7 @@ CREATE TABLE employees (
   last_name VARCHAR(30) NOT NULL,
   role_id INTEGER NOT NULL,
   manager_id INTEGER,
+  is_manager BOOLEAN DEFAULT false,
   PRIMARY KEY (id)
 );
 
@@ -36,11 +37,6 @@ VALUES ("Minister of Magic", 1),("Unspeakable", 8),("Head of Department of Magic
 INSERT INTO roles (title, salary, department_id)
 VALUES ("Auror", 150000.00, 2), ("Improper Use of Magic Officer", 70000.00, 2), ("Wizengamot Member", 100000.00, 2), ("Magical Law Enforcement Squad", 70000.00, 2),("Misuse of Muggle Artefacts Officer", 65000.00, 2),("Detection and Confiscation of Counterfeit Defensive Spells and Protective Objects Officer", 50000.00, 2),("Accidental Magic Reversal Squad", 75000.00, 3),("Obliviator", 100000.00, 3),("Muggle-Worthy Excuse Commitee Member", 50000.00, 3),("Beast Division", 30000.00, 4),("Being Division", 30000.00, 4),("Spirit Division", 30000.00, 4),("International Magical Trading Standards Body Member", 100000.00, 5),("International Magical Lawyer", 100000.00, 5),("International Confederation of Wizards Member", 100000.00, 5),("Floo Network Authority", 60000.00, 6),("Broom Regulatory Committee Member", 50000.00, 6),("Portkey Office Member", 45000.00, 6),("Apparition Test Centre Member", 40000.00, 6),("British and Irish Quidditch League Headquarters Member", 60000.00, 7),("Official Gobstones Club Member", 30000.00, 7),("Ludicrous Patents Office Member", 40000.00, 7),("Senior Undersecretary to the Minister of Magic", 80000.00, 1),("Magical Maintenance Worker", 20000.00, 9);
 
--- first_name VARCHAR(30),
---   last_name VARCHAR(30) NOT NULL,
---   role_id INTEGER NOT NULL,
---   manager_id INTEGER,
-
 -- Perkins is actually head of the Misuse of Muggle Artefacts Office
 INSERT INTO employees(last_name, role_id)
 VALUES ("Perkins", 13);
@@ -50,11 +46,11 @@ VALUES ("Perkins", 13);
 -- Bob Ogden was head of the Magical Law Enforcement Squad at one point
 -- some of these employees are dead
 -- employees without managers
-INSERT INTO employees (first_name, last_name, role_id)
-VALUES ("Cornelius", "Fudge", 1),("Rufus", "Scrimgeor", 1),("Harry", "Potter", 3),("Arthur", "Weasley", 14),("Barty", "Crouch Sr.", 6),("Ludo", "Bagman", 8),("Dolores", "Umbridge", 31), ("Broderick", "Bode", 2),("Reginald", "Cattermole", 32),("Dirk", "Cresswell", 19),("Amos", "Diggory", 18),("Madam", "Edgecombe", 24),("Newt", "Scamander", 18),("Amelia", "Bones", 3),("Pius", "Thicknesse", 3);
+INSERT INTO employees (first_name, last_name, role_id, is_manager)
+VALUES ("Cornelius", "Fudge", 1, true),("Rufus", "Scrimgeor", 1, true),("Harry", "Potter", 3, true),("Arthur", "Weasley", 14, true),("Barty", "Crouch Sr.", 6, true),("Ludo", "Bagman", 8, true),("Broderick", "Bode", 2, false),("Reginald", "Cattermole", 32, false),("Dirk", "Cresswell", 19, false),("Amos", "Diggory", 18, false),("Madam", "Edgecombe", 24, false),("Newt", "Scamander", 18, false),("Amelia", "Bones", 3, true),("Pius", "Thicknesse", 3, true);
 
 -- also heads of magical law enforcement
 
 -- employees with managers
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
-VALUES ("Kingsley", "Shacklebolt", 9, 3),("Nymphadora", "Tonks", 9, 15),("Alastor", "Moody", 9, 16),("Frank", "Longbottom", 9, 15),("Alice", "Longbottom", 9, 15),("John", "Dawlish", 9, 3),("Gawain", "Robards", 9, 3),("Hesphaestus", "Gore", 9, 3),("Mafalda", "Hopkirk", 10, 3),("Albus", "Dumbledore", 11, 16),("Percy", "Weasley", 11, 3),("Griselda", "Marchbanks", 11, 3),("Bob", "Ogden", 12, 3),("Tiberius", "Ogden", 11, 3),
+VALUES ("Kingsley", "Shacklebolt", 9, 3),("Nymphadora", "Tonks", 9, 15),("Alastor", "Moody", 9, 16),("Frank", "Longbottom", 9, 15),("Alice", "Longbottom", 9, 15),("John", "Dawlish", 9, 3),("Gawain", "Robards", 9, 3),("Hesphaestus", "Gore", 9, 3),("Mafalda", "Hopkirk", 10, 3),("Albus", "Dumbledore", 11, 16),("Percy", "Weasley", 11, 3),("Griselda", "Marchbanks", 11, 3),("Bob", "Ogden", 12, 15),("Tiberius", "Ogden", 11, 16),("Dolores", "Umbridge", 31, 1), 
--- server.js
+++ server.js
@@ -46,10 +46,10 @@ const start = () => {
 
     switch (actionType) {
       case "view":
-        askViewAddQ(actionType);
+        askViewQ();
       break;
-      case "sdd":
-        askViewAddQ(actionType);
+      case "add":
+        askAddQ();
       break;
       case "update":
         askUpdateQ();
@@ -58,11 +58,12 @@ const start = () => {
   });
 }
 
-const askViewAddQ = (actionType) => {
+//ask what they'd like to view
+const askViewQ = () => {
   inquirer.prompt(
     {
       type: "list",
-      message: `What would you like to ${actionType}?`,
+      message: "What would you like to view?",
       name: "category",
       choices: [
         "Employee",
@@ -76,17 +77,54 @@ const askViewAddQ = (actionType) => {
     switch (category) {
       case "employee":
         console.log("employee");
+        askEmployeeViewQ();
       break;
       case "role":
         console.log("role");
+        showRoles();
       break;
       case "department":
         console.log("department");
+        showDepartments();
       break;
     }
   });
 }
 
+//ask what they'd like to add
+const askAddQ = () => {
+  inquirer.prompt(
+    {
+      type: "list",
+      message: "What would you like to add?",
+      name: "category",
+      choices: [
+        "Employee",
+        "Role",
+        "Department"
+      ]
+    },
+  ).then((response) => {
+    let category = (response.category).toLowerCase();
+
+    switch (category) {
+      case "employee":
+        console.log(`employee`);
+        addEmployee();
+      break;
+      case "role":
+        console.log(`role`);
+        addRole();
+      break;
+      case "department":
+        console.log(`department`);
+        addDepartment();
+      break;
+    }
+  });
+}
+
+//ask what they'd like to update
 const askUpdateQ = () => {
   inquirer.prompt(
     {
@@ -104,10 +142,90 @@ const askUpdateQ = () => {
     switch (category) {
       case "employee role":
         console.log("employee role");
+        updateRole();
       break;
       case "employee manager":
         console.log("employee manager");
+        updateManager();
       break;
     }
   });
+}
+
+//ask if they'd like to view employee by department, role, manager
+const askEmployeeViewQ = () => {
+  inquirer.prompt(
+    {
+      type: "list",
+      message: "By what category would you like to view employees?",
+      name: "category",
+      choices: [
+        "Role",
+        "Department",
+        "Manager"
+      ]
+    },
+  ).then((response) => {
+    let category = (response.category).toLowerCase();
+
+    switch (category) {
+      case "role":
+        console.log("role");
+        //get roles from db
+      break;
+      case "department":
+        console.log("department");
+        //get departments from db
+      break;
+      case "manager":
+        console.log("manager");
+        //get managers from db
+      break;
+    }
+  });
+}
+
+const showRoles = () => {
+  //get roles
+  //ask if they'd like to add a role, view employees by role, or exit
+}
+
+const showDepartments = () => {
+  //get departments
+  //ask if they'd like to add a department, view employees by department, or exit
+}
+
+const addEmployee = () => {
+  //ask for first_name, last_name, role, is_manager
+  //match role to role_id
+  //if is_manager = false, check if department has a manager
+  //if department has a manager, add manager_id
+  //add to db
+}
+
+const addRole = () => {
+  //ask for title, salary, department
+  //match department to department_id
+  //add to db
+}
+
+const addDepartment = () => {
+  //ask for name
+  //add to db
+}
+
+const updateRole = () => {
+  //ask employee first_name, last_name
+  //show employee info
+  //ask what the new role is
+  //show employee info
+  //update db
+}
+
+const updateManager = () => {
+  //ask employee first_name, last_name
+  //show employee info
+  //ask name of new manager
+  //show employee info
+  //update db
 }
